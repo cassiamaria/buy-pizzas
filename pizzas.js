642e59d7290738c2c91baaaad0e9c4440630454f
Cassia Maria <cassiamariaassis@gmail.com>
2020-09-30 07:06:45 -0300

    Initial commit

---
+++ pizzas.js
@@ -0,0 +1,8 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Molho de tomate, queijo tipo mussarela, azeitonas pretas e orégano'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Linguiça calabresa, rodelas de cebolas frescas, azeitonas pretas, mussarela, polpa de tomate e   orégano'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Molho de tomate, quatro queijos, azeitonas pretas e orégano'},
+    {id:4, name:'Nordestina', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Molho de tomate, carne de sol desfiada, catupiry e orégano'},
+    {id:5, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Molho de tomate, queijo cheddar, picada e borda de catupiry'},
+    {id:6, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Coberta com brigadeiro caseiro da casa'},
+];
